c8357463886323eef786f96b1a5a5d676d00abf9
toluOjo <ojotolulope137@gmail.com>
2023-11-24 10:14:28 +0100

    product-service

---
+++ src/main/java/com/example/productservice/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.example.productservice.service;
+
+
+import com.example.productservice.dto.ProductRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public void createProduct(ProductRequest productRequest){
+
+    }
+}
